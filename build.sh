e9409cc89d3a4fb2507c106d2c4e17951b11618c
sloev <johannesgj@gmail.com>
2015-10-27 11:21:19 +0000

    mod build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ echo "removing old container"
 docker rm mailpile_con >> buildlog.log
 echo "rebuilding klinisk_api_img"
 docker build --rm -t mailpile_img . >> buildlog.log
-echo "running klinisk_api_con as daemon, port 5000 forwarded!!!"
-docker run -d -p 5000:5000 --name klinisk_api_con klinisk_api_img >> buildlog.log
+echo "running mailpile_con as daemon, port 5000 forwarded!!!"
+docker run -d --name mailpile_con -p 33411:33411 mailpile_img
 echo "finnished"
-docker run -d --name mailpile_con -p 33411:33411 mailpile_test_img
 
