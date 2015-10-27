0e7fd387fe90b0ba41366bb440322faacf77e972
sloev <johannesgj@gmail.com>
2015-10-27 12:07:33 +0000

    nmothing here

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ docker rm mailpile_con >> buildlog.log
 echo "rebuilding mailpile_img"
 docker build --rm -t mailpile_img . >> buildlog.log
 echo "running mailpile_con as daemon, port 39411 forwarded!!!"
-docker run -d --name mailpile_con -v /root/mailpile_data:/mailpile-data/.local/share/Mailpile -p 33411:33411 mailpile_img
+docker run -d --name mailpile_con -v /var/mailpile:/mailpile-data/.local/share/Mailpile -p 33411:33411 mailpile_img
 echo "finnished"
 
