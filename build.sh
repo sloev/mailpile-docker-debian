36c026e7f9fdb7f1edabb5271733c6c69b5312f0
sloev <johannesgj@gmail.com>
2015-10-27 11:32:59 +0000

    added vollume /root/mailpile_data

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ docker rm mailpile_con >> buildlog.log
 echo "rebuilding klinisk_api_img"
 docker build --rm -t mailpile_img . >> buildlog.log
 echo "running mailpile_con as daemon, port 5000 forwarded!!!"
-docker run -d --name mailpile_con -p 33411:33411 mailpile_img
+docker run -d --name mailpile_con -v /root/mailpile_data:/mailpile-data/.local/share/Mailpile -p 33411:33411 mailpile_img
 echo "finnished"
 
