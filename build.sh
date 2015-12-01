4d1ac96078d89c23f84dce5505533ad5fdd0a95f
sloev <johannes.valbjorn@gmail.com>
2015-12-01 14:55:03 +0100

    changed to port 80

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ echo "removing old container"
 docker rm mailpile_con >> buildlog.log
 echo "rebuilding mailpile_img"
 docker build --rm -t mailpile_img . >> buildlog.log
-echo "running mailpile_con as daemon, port 39411 forwarded!!!"
-docker run -d --name mailpile_con -v /var/mailpile:/root/.local/share/Mailpile -p 33411:33411 mailpile_img
+echo "running mailpile_con as daemon, port 80 forwarded!!!"
+docker run -d --name mailpile_con -v /var/mailpile:/root/.local/share/Mailpile -p 80:33411 mailpile_img
 echo "finnished"
 
 
