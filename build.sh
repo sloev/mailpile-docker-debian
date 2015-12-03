7eda36379c3b21ac127a2c2c405595d623cbebba
sloev <johannes.valbjorn@gmail.com>
2015-12-03 13:33:38 +0100

    added remove untagged

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 echo "rebuilding and running, check buildlog.log for details"
+
+echo "removing all untagged docker images"
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
 echo "" > buildlog.log
 echo "stopping old container"
 docker stop mailpile_con >> buildlog.log
@@ -9,5 +12,3 @@ docker build --rm -t mailpile_img . >> buildlog.log
 echo "running mailpile_con as daemon, port 80 forwarded!!!"
 docker run -d --name mailpile_con -v /var/mailpile:/root/.local/share/Mailpile -p 80:33411 mailpile_img
 echo "finnished"
-
-
